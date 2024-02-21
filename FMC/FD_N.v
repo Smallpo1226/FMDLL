742f6152adc7da0ad7a89fd647b55eac46d6dfc5
Smallpo1226 <135593440+Smallpo1226@users.noreply.github.com>
2024-02-21 18:14:21 +0800

    modified

---
+++ FMC/FD_N.v
@@ -0,0 +1,11 @@
+module FD_M( rst_n , N , DIV_N , CLK_out );
+    reg [2:0]cnt_tmp;
+    reg clk_tmp;
+    reg [2:0]cnt;
+	input [2:0]N;
+	input rst_n;
+    input CLK_out;
+	output reg DIV_N;
+
+
+endmodule
---
+++ FMC/FMC.v
@@ -0,0 +1,21 @@
+module FMC (CLK_exit,CLK_out,rst_n,Sel,DIV_M);
+input CLK_exit;
+input CLK_out;
+input rst_n;
+output [1:0]Sel;
+output DIV_M;
+
+wire DIV_N;
+
+FD_M F0(.M(M),.DIV_M(DIV_M),.CLK_exit(CLK_exit),.rst_n(rst_n));
+FD_N F1(.N(N),.DIV_N(DIV_N),.CLK_out(CLK_out),.rst_n(rst_n));
+Select_Logic F2();
+
+
+
+
+
+
+
+
+endmodule
