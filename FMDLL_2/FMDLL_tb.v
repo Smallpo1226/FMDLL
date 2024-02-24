97d0d07a342f99c76c923794c31d27bce48c8bf9
Smallpo1226 <135593440+Smallpo1226@users.noreply.github.com>
2024-02-24 16:57:32 +0800

    Add files via upload

---
+++ FMDLL_2/FMDLL_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns/100ps
+module FMDLL_tb();
+    reg [1:0] Sel;
+    reg [1:0] M;
+    reg [3:0] N;
+    reg CLK_exit;
+    reg rst_n;
+    wire CLK_out;    
+    FMDLL u1(.M(M),
+           .N(N),
+           .CLK_exit(CLK_exit),
+           .CLK_out(CLK_out),
+           .rst_n(rst_n)
+           );
+  
+
+    initial begin
+      CLK_exit= 1;
+      N      = 4'd10;
+      M      = 2'd3;
+      rst_n  = 1;
+      #10
+      rst_n  = 0;
+      #200
+      $finish;
+    end
+
+    always #(1 / 2) CLK_exit = ~CLK_exit;
+    
+     initial begin
+        $fsdbDumpfile("../4.Simulation_Result/FMDLL_rtl.fsdb");
+        $fsdbDumpvars;
+    end
+
+
+
+endmodule
---
+++ FMDLL_2/HLD_Ctrl2.v
@@ -0,0 +1,22 @@
+`include "FD_2.v"
+module HLD_Ctrl2 (
+    input clk2,
+    input clk4,
+    input M,
+    input DIV_M,
+    input rst_n,
+    output reg Ctrl_HLD2
+);
+    wire clk2b;
+    wire clk2b_div2;
+
+    assign clk2b = ~clk2;
+    FD_2 F0 (.clk(clk2b), .rst_n(rst_n), .DIV_2(clk2b_div2));
+    always @(*) begin
+        if (M == 1) begin
+            Ctrl_HLD2 = ~((~clk2b_div2) | clk2b);
+        end else begin
+            Ctrl_HLD2 = ~((~(~((~clk4) | clk2))) | DIV_M);
+        end
+    end
+endmodule
