9454a23df89ea11669b16c5aa48af6ce2d60d7ac
andyhandsome666 <andy.lin61013@gmail.com>
2024-02-26 23:39:11 +0800

    FDL modified

--- FDL.v
+++ FDL.v
@@ -1,17 +1,16 @@
-`include "FDE.v"
 module FDL (
-    input clk_in,
     input [5:0] Q,
-    input [5:0] Qb,
+    input clk_in,
     output clk_out
 );
-INVXL I1(.A(clk_in),.Y(connect));
-INVXL I2(.A(connect),.Y(clk_out));
-FDE X1 (.Q(Q[0]),.Qb(Qb[0]),.connect(connect),.feedback(clk_out));
-FDE X2 (.Q(Q[1]),.Qb(Qb[1]),.connect(connect),.feedback(clk_out));
-FDE X4 (.Q(Q[2]),.Qb(Qb[2]),.connect(connect),.feedback(clk_out));
-FDE X8 (.Q(Q[3]),.Qb(Qb[3]),.connect(connect),.feedback(clk_out));
-FDE X16(.Q(Q[4]),.Qb(Qb[4]),.connect(connect),.feedback(clk_out));
-FDE X32(.Q(Q[5]),.Qb(Qb[5]),.connect(connect),.feedback(clk_out));
+    wire [5:0] dummy_wire;
+    NAND2X8 FDE1 (.A(clk_in), .B(Q[0]), .Y(dummy_wire[0]));
+    NAND2X8 FDE1 (.A(clk_in), .B(Q[1]), .Y(dummy_wire[1]));
+    NAND2X8 FDE1 (.A(clk_in), .B(Q[2]), .Y(dummy_wire[2]));
+    NAND2X8 FDE1 (.A(clk_in), .B(Q[3]), .Y(dummy_wire[3]));
+    NAND2X8 FDE1 (.A(clk_in), .B(Q[4]), .Y(dummy_wire[4]));
+    NAND2X8 FDE1 (.A(clk_in), .B(Q[5]), .Y(dummy_wire[5]));
+
+    assign clk_out = clk_in;
 
 endmodule
