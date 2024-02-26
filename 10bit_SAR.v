6b24013acab8682cce3a20f40a2d2f65d8663871
andyhandsome666 <andy.lin61013@gmail.com>
2024-02-26 20:09:04 +0800

    2/26

--- 10bit_SAR.v
+++ 10bit_SAR.v
@@ -1,37 +1,32 @@
-module SAR(
+module SAR (
     input COMP,
     input clk4,
     input rst_n,
     output reg [9:0] Q
-    );
+);
     
     reg [9:0] Q_next;
     reg [3:0] count_next,count;
 
     always@* begin
 
-		Q_next=Q;
-        count_next=count;
+		Q_next = Q;
+        count_next = count;
 
-        if(COMP==1'b0) begin    //lag
-            if(count!=0) begin
-                Q_next[count]=0;
-                Q_next[count-1]=1;
-                Q_next=count- 4'd1;
+        if (COMP == 1'b0) begin    //lag
+            if (count != 0) begin
+                Q_next[count] = 0;
+                Q_next[count-1] = 1;
+                count_next = count - 4'd1;
             end
-            else    
-            if(count==0)begin
-                Q_next[0]= 0;
+            else if (count == 0) begin
+                Q_next[0] = 0;
             end    
-        end                 
-		else  
-        if(COMP) begin    //lead
-            if(count!=0)begin
-                Q_next[count-1]=1;
-                count_next=count- 4'd1;
-            end
-            else
-            if(count==0)begin
+        end else if (COMP) begin    //lead
+            if (count != 0) begin
+                Q_next[count-1] = 1;
+                count_next = count - 4'd1;
+            end else if(count == 0)begin
                 Q_next[0]= 1;
             end
         end
