0edc2be013df6eab22eba2b3a623ed9a58822ccf
Smallpo1226 <135593440+Smallpo1226@users.noreply.github.com>
2024-03-18 22:43:46 +0800

    modify_FD_N

--- FD_N.v
+++ FD_N.v
@@ -4,14 +4,21 @@ input clk_out;
 input rst_n;
 input [3:0] N_counter;
 output reg DIV_N;
+reg DIV_N_tmp;
 
- always@(posedge clk_out or negedge rst_n) begin
+ always@(posedge clk_out or negedge rst_n)begin
    if (!rst_n)
-     DIV_N <= 1;
-   else if (N_counter == N-4'd1)
-     DIV_N <= 0;
-   else if (N_counter == N)
-     DIV_N <= 1;
+    DIV_N <= 0;
+   else 
+    DIV_N <= DIV_N_tmp;
+ end
+ always @* begin
+  if (N_counter == N || N_counter == 3'd0)
+    DIV_N_tmp = 1;
+  else if (N_counter == N-1)
+    DIV_N_tmp = 0;
+  else
+    DIV_N_tmp = 1;
  end
 
- endmodule
+endmodule
